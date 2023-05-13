91e8edd6902f2b052be7a4457dbd952ac2518aae
k1Dapt <k1Dapt@cyber-dojo.org>
2023-05-13 19:27:44 +0000

    11 ran tests, predicted amber, got amber

--- files/fizz_buzz_steps.js
+++ files/fizz_buzz_steps.js
@@ -5,14 +5,30 @@ const assert = require('assert');
 const fizz_buzz = require('./fizz_buzz');
 
 let score;
-
-Given(/^the hitch-hiker selects some tiles$/, function() {
+Given('we have a FizzBuzz translator', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
 });
 
-When(/^they spell (\d+) times (\d+)$/, function(a, b) {
-  score = fizz_buzz.answer(parseInt(a), parseInt(b));
+When('we give it {int}', function (int) {
+  // When('we give it {float}', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
 });
 
-Then(/^the score is (\d+)$/, function(expectedScore) {
-  assert.equal(score, expectedScore);
+Then('we expect {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
 });
+
+//Given(/^the hitch-hiker selects some tiles$/, function() {
+//});
+//
+//When(/^they spell (\d+) times (\d+)$/, function(a, b) {
+//  score = fizz_buzz.answer(parseInt(a), parseInt(b));
+//});
+//
+//Then(/^the score is (\d+)$/, function(expectedScore) {
+//  assert.equal(score, expectedScore);
+//});
+//
--- files/readme.txt
+++ files/readme.txt
@@ -1,4 +1,9 @@
 Write a program that prints the numbers from 1 to 100, but...
+    Rules: For any whole number between 1 and 100 ...
+    Multiples of 3 only, or numbers containing 3, return "Fizz",
+    Multiples of 5 only, or numbers containing 5, return "Buzz",
+    Multiples of both, or numbers containing both, or both cases, return "FizzBuzz"
+    Multiples of neither, return the given mumber, as a string
 
 numbers that are exact multiples of 3, or that contain 3, must print a string containing "Fizz"
    For example 9 -> "...Fizz..."
